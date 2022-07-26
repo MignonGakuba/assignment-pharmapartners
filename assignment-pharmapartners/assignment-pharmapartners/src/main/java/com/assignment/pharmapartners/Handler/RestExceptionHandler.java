c88d48608579f2dac2e9ef118111208c3c3d7610
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-07-26 12:27:46 +0200

    Create RestExceptionHandler.java

--- assignment-pharmapartners/assignment-pharmapartners/src/main/java/com/assignment/pharmapartners/Handler/RestExceptionHandler.java
+++ assignment-pharmapartners/assignment-pharmapartners/src/main/java/com/assignment/pharmapartners/Handler/RestExceptionHandler.java
@@ -1,14 +1,27 @@
 package com.assignment.pharmapartners.Handler;
 
+import com.assignment.pharmapartners.helper.ErrorCode;
+import com.assignment.pharmapartners.helper.JsonResult;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.persistence.EntityNotFoundException;
 
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
 
 
 
