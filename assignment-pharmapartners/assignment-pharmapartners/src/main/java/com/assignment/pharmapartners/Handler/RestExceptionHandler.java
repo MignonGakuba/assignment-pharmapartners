bdae0146770c02b6064e74cc86063f856948d44d
MignonGakuba <36228508+MignonGakuba@users.noreply.github.com>
2022-07-26 12:17:36 +0200

    Create RestExceptionHandler.java

---
+++ assignment-pharmapartners/assignment-pharmapartners/src/main/java/com/assignment/pharmapartners/Handler/RestExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.assignment.pharmapartners.Handler;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+
+}
